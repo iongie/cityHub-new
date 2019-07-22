29a6a08d1b7c803483d87cbc832893b2170c21b2
Novranz <44194749+novransyah@users.noreply.github.com>
2019-07-22 15:21:07 +0700

    a
    a

---
+++ src/app/pages/main/tezz/a/a.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AComponent } from './a.component';
+
+@NgModule({
+  declarations: [AComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AModule { }
