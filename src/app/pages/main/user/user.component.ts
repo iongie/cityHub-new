2ff909c57f3f677561fd965a2423fc504a964e78
Gigih <gigih985042@gmail.com>
2019-07-22 14:27:25 +0700

    update
    update

--- src/app/pages/main/user/user.component.ts
+++ src/app/pages/main/user/user.component.ts
@@ -119,23 +119,23 @@ export class UserComponent implements OnInit, OnDestroy {
           this.user = new LocalDataSource (data);
         });
 
-        this.dataFilter = filter.map((forFilter) => {
-          const DataForFilter = {
-            createPermission: forFilter.create_permision,
-            readPermission: forFilter.read_permision,
-            updatePermission: forFilter.update_permision,
-            deletePermission: forFilter.delete_permision,
-            moduleDescription: forFilter.module_description,
-            moduleId: forFilter.module_id,
-            moduleName: forFilter.module_name,
-            privilegeDescription: forFilter.privilege_description,
-            privilegeId: forFilter.privilege_id,
-            privilegeName: forFilter.privilege_name,
-            userRoleId: forFilter.user_role_id,
-          };
-          return DataForFilter;
-        });
-        console.log('filter for module user', this.dataFilter);
+        // this.dataFilter = filter.map((forFilter) => {
+        //   const DataForFilter = {
+        //     createPermission: forFilter.create_permision,
+        //     readPermission: forFilter.read_permision,
+        //     updatePermission: forFilter.update_permision,
+        //     deletePermission: forFilter.delete_permision,
+        //     moduleDescription: forFilter.module_description,
+        //     moduleId: forFilter.module_id,
+        //     moduleName: forFilter.module_name,
+        //     privilegeDescription: forFilter.privilege_description,
+        //     privilegeId: forFilter.privilege_id,
+        //     privilegeName: forFilter.privilege_name,
+        //     userRoleId: forFilter.user_role_id,
+        //   };
+        //   return DataForFilter;
+        // });
+        // console.log('filter for module user', this.dataFilter);
       });
     });
   }
