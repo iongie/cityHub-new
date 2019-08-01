02d49e0562ffb6d91b295d6842ecca02476c28a0
Gigih <gigih985042@gmail.com>
2019-08-01 10:16:40 +0700

    update room tariff
    update room tariff

--- src/app/pages/main/role/role.component.ts
+++ src/app/pages/main/role/role.component.ts
@@ -1,8 +1,10 @@
+import { PrivilegeService } from './../../../services/privilege/privilege.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserRoleService } from '../../../services/user-role/user-role.service';
 import { NotificationService } from '../../../services/notification/notification.service';
 import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-role',
@@ -15,9 +17,13 @@ export class RoleComponent implements OnInit, OnDestroy {
     public notifServ: NotificationService,
     public router: Router,
     public userRoleServ: UserRoleService,
+    public privilegeServ: PrivilegeService,
   ) { }
 
   ngOnInit() {
+    this.getRole();
+    this.getPrivilege();
+    this.refreshRole();
   }
 
   ngOnDestroy() {
@@ -25,13 +31,32 @@ export class RoleComponent implements OnInit, OnDestroy {
     this.subs.complete();
   }
 
-  refreshRoom() {
+  refreshRole() {
     this.userRoleServ.refresh.subscribe(() => {
       this.getRole();
     });
   }
 
   getRole() {
+
+  }
+
+  getPrivilege() {
+    this.privilegeServ.get().pipe(takeUntil(this.subs)).subscribe(resPrivilege => {
+      const ret = resPrivilege.map((forResPrivilege) => {
+        const dataResPrivilege = {
+          privilegeId: forResPrivilege.privilege_id,
+          privilegeName: forResPrivilege.privilege_name,
+          privilegeDesc: forResPrivilege.privilege_description,
+        };
+        return dataResPrivilege;
+      });
+      console.log('return privilege', ret);
+      console.log('privilege of server', resPrivilege);
+    });
+  }
+
+  filterByPrivilege() {
 
   }
 
