8eb57f384bd183d6e3a8005014f16d78a29c3a66
Gigih <gigih985042@gmail.com>
2019-08-08 16:00:38 +0700

    commit - update booking code
    update booking code

--- src/app/pages/main/booking/booking.component.ts
+++ src/app/pages/main/booking/booking.component.ts
@@ -8,6 +8,7 @@ import { BusinessSourceService } from './../../../services/business-source/busin
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
 import { takeUntil, map, debounceTime } from 'rxjs/operators';
+import { CountryService } from '../../../services/country/country.service';
 
 @Component({
   selector: 'ngx-booking',
@@ -18,41 +19,60 @@ export class BookingComponent implements OnInit, OnDestroy {
   private subs: Subject<void> = new Subject();
   businessSource: any;
   guest: any;
+  country: any;
+  userCityHub: any;
   bookingStepOne = {
     arrivalDate: '',
     duration: '',
     guestId: '',
+    guestName: '',
+    address: '',
+    city: '',
+    countryId: '',
+    guestFileScan: '',
+    phoneNumber: '',
     sourceId: '',
+    sourceName: '',
+    sourceDesc: '',
     createdBy: '',
   }
 
-  public model: any;
-  search = (text$: Observable<string>) =>
-    text$.pipe(
-      debounceTime(200),
-      map(term => term === '' ? []
-        : this.guest.filter(v => v.guestName.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
-    )
+  modelGuest: any;
+  modelBusinessSource: any;
 
-  formatter = (x: {name: string}) => x.name;
   constructor(
     public businessSourceServ: BusinessSourceService,
     public guestServ: GuestService,
     public notificationServ: NotificationService,
     public userRoleServ: UserRoleService,
-    public uthServ: AuthService,
+    public authServ: AuthService,
     public roomTypeServ: RoomTypeService,
     public roomOperationServ: RoomOperationService,
+    public countryServ: CountryService,
   ) { }
 
   ngOnInit() {
     this.getGuest();
     this.getBusinessSource();
+    this.getCountry();
+    this.detailAccount();
   }
 
   ngOnDestroy() {
     this.subs.next();
     this.subs.complete();
+  }
+
+  detailAccount() {
+    const data = {
+      token: localStorage.getItem('p_l1oxt'),
+    };
+    this.authServ.detailAfterLogin(data).pipe(takeUntil(this.subs)).subscribe(res => {
+      this.userCityHub = {
+        name : res[0].full_name,
+      };
+      console.log(res);
+    });
   }
 
   getBusinessSource() {
@@ -87,8 +107,120 @@ export class BookingComponent implements OnInit, OnDestroy {
     });
   }
 
-  doSomething(event) {
+  getCountry() {
+    this.countryServ.get().pipe(takeUntil(this.subs)).subscribe(resCountry => {
+      this.country = resCountry.map((y) => {
+        const xyz = {
+          countryId: y.country_id,
+          countryName: y.country_name,
+        };
+        return xyz;
+      })
+    })
+  }
+
+  searchGuest = (textGuest: Observable<string>) =>
+  textGuest.pipe(
+      debounceTime(200),
+      map(term => term === '' ? []
+        : this.guest.filter(v => v.guestName.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+    )
+
+  formatterGuest = (x: {guestName: string}) => x.guestName;
+
+  searchBusinessSource = (textBusinessSource: Observable<string>) =>
+  textBusinessSource.pipe(
+      debounceTime(200),
+      map(term => term === '' ? []
+        : this.businessSource.filter(v => v.businnessSourceName.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+    )
+
+  formatterBusinessSource =(x: {businnessSourceName: string}) => x.businnessSourceName;
+
+  onChangeGuest(event) {
+    if(event === '') {
+      this.bookingStepOne = {
+        arrivalDate: '',
+        duration: '',
+        guestId: '',
+        guestName: '',
+        address: '',
+        city: '',
+        countryId: '',
+        guestFileScan: '',
+        phoneNumber: '',
+        sourceId: '',
+        sourceName: '',
+        sourceDesc: '',
+        createdBy: '',
+      }
+    } else {
+      this.bookingStepOne = {
+        arrivalDate: '',
+        duration: '',
+        guestId: event.guestId,
+        guestName: event.guestName,
+        address: event.address,
+        city: event.city,
+        countryId: event.countryId,
+        guestFileScan: event.guestFileScan,
+        phoneNumber: event.phoneNumber,
+        sourceId: '',
+        sourceName: '',
+        sourceDesc: '',
+        createdBy: '',
+      }
+    }
+    
     console.log(event);
+  }
+
+  onChangeBusinessSource(event) {
+    if(event === '') {
+      this.bookingStepOne = {
+        arrivalDate: '',
+        duration: '',
+        guestId: '',
+        guestName: '',
+        address: '',
+        city: '',
+        countryId: '',
+        guestFileScan: '',
+        phoneNumber: '',
+        sourceId: '',
+        sourceName: '',
+        sourceDesc: '',
+        createdBy: '',
+      }
+    } else {
+      this.bookingStepOne = {
+        arrivalDate: '',
+        duration: '',
+        guestId: this.bookingStepOne.guestId,
+        guestName: this.bookingStepOne.guestName,
+        address: this.bookingStepOne.address,
+        city: this.bookingStepOne.city,
+        countryId: this.bookingStepOne.countryId,
+        guestFileScan: this.bookingStepOne.guestFileScan,
+        phoneNumber: this.bookingStepOne.phoneNumber,
+        sourceId: event.businnessSourceId,
+        sourceName: event.businnessSourceName,
+        sourceDesc: event.businnessSourceDescription,
+        createdBy: '',
+      }
+    }
+    
+    console.log(this.bookingStepOne);
+  }
+
+  saveStepOne() {
+    const data = {
+      arrivalDate: '',
+      duration: '',
+      guestId: this.bookingStepOne.guestId,
+      sourceId: this.bookingStepOne.sourceId,
+      createBy: this.userCityHub.username,
+    };
   }
 
 }
