807c644a59e7a60bc18101591a0efd65058983f7
Novranz <44194749+novransyah@users.noreply.github.com>
2019-08-14 11:15:34 +0700

    a
    a

---
+++ src/app/pages/main/dashboard/rooms/room-maps/room-maps.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RoomMapsComponent } from './room-maps.component';
+
+@NgModule({
+  declarations: [RoomMapsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RoomMapsModule { }
